1358f5306b489cff6a38a45bfa357da1b56945fd
Maarten van Wersch <66728012+maartenvanwersch@users.noreply.github.com>
2024-01-03 19:15:11 +0100

    refactored

--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -1,12 +1,15 @@
 def evaluate(secret, guess):
-    correct_guessed_indices = []
-    filtered_guess_list = guess.copy()
-    filtered_secret_list = secret.copy()
-    for count, value in enumerate(secret):
-        if len(guess) > count and guess[count] == value:
-            correct_guessed_indices.append(count)
-            filtered_guess_list.remove(value)
-            filtered_secret_list.remove(value)
-    misplaced_list = [value for value in filtered_guess_list if value in filtered_secret_list]
+    correct_guessed_indices = find_matching_positions(secret, guess)
+    filtered_secret = remove_indices(secret, correct_guessed_indices)
+    filtered_guess = remove_indices(guess, correct_guessed_indices)
+    misplaced_list = [value for value in filtered_guess if value in filtered_secret]
     return [len(correct_guessed_indices), len(misplaced_list)]
+
+
+def find_matching_positions(list1, list2):
+    return [i for i, (x, y) in enumerate(zip(list1, list2)) if x == y]
+
+
+def remove_indices(original_list, indices_to_remove):
+    return [item for i, item in enumerate(original_list) if i not in indices_to_remove]
 
